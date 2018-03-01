ab9ac0ee5f542c25587c655dc27385a208354926
blackyani <smert4ekistam@gmail.com>
2018-03-01 18:06:38 +0200

    first commit

---
+++ src/app/shared/components/loader/loader.component.ts
@@ -0,0 +1,31 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'wfm-loader',
+  template: `<div class='loader loader4'>
+    <div>
+      <div>
+        <div>
+          <div>
+            <div>
+              <div>
+                <div>
+                  <div>
+                    <div>
+                      <div></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`,
+  styleUrls: ['./loader.component.scss']
+})
+
+export class LoaderComponent {
+
+}
